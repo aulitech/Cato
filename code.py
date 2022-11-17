287d0a5c745884d562e7a4b03d25e0cde59283b3
Finn Biggs <Finn@auli.tech>
2022-11-17 12:03:28 -0800

    add async structuring update readme

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ import io
 import analogio
 import digitalio
 import busio
+import asyncio
 
 import battery
 
@@ -22,58 +23,66 @@ import asyncio
 
 #import os
 
-print("\n")
+# print("\n")
 
-#Wait for user to confirm readiness (calibration, readability)
-while False:
-    try:
-        print("Waiting to begin (CtrC)")
-        time.sleep(5)
-    except KeyboardInterrupt:
-            break
+# #Wait for user to confirm readiness (calibration, readability)
+# while False:
+#     try:
+#         print("Waiting to begin (CtrC)")
+#         time.sleep(5)
+#     except KeyboardInterrupt:
+#             break
 
+async def battery_process():
+    # read the battery info
+    # STUB
+    asyncio.sleep(30)
 
-print_boot_out = False
-if(print_boot_out == True):
-    print("SETUP INFORMATION: ")
+async def feed_dog():
+    ''' feed the watchdog '''
+    asyncio.sleep(20)
+
+async def loop():
+    ''' docstring '''
+    while True:
+        asyncio.sleep(2)
+
+def print_boot_out():
     print("boot_out.txt: ")
     with io.open("boot_out.txt") as b: 
         for line in b.readlines():
             print('\t', line, end='')
-        b.close()
 
 async def main():
-    print("Initializing Cato - interrupt will cause error")
-    try:
-        c = Cato.Cato( bt=True )
-        print("Initialization complete.")
-    except KeyboardInterrupt:
-        print("\tinterrupted during initialization")
-        pass
+    # print_boot_out()
+    c = Cato( bt = True )
 
-    while True:
-        try:
-            await asyncio.sleep(0.1)
-            c.blue.battery_service.level = c.battery.get_percent()
-            c.move_mouse() #instead of moving mouse as method, have it be blocked on async when not in use
-            c.dispatch_event( c.detect_event() ) # dispatch event should always be running
-        except:
-            break
+    tasks = []
+    tasks.append( asyncio.create_task( battery_process() ) )
+    tasks.append( asyncio.create_task( loop() ) )
+    tasks.append( asyncio.create_task( feed_dog() ) )
+    await asyncio.gather( *tasks )
 
-# async this loop to be await async.sleep(
-# bat = battery.Bat()
-# ti = time.time()
-# try:
+# async def main():
+#     print("Initializing Cato - interrupt will cause error")
+#     try:
+#         c = Cato.Cato( bt=True )
+#         print("Initialization complete.")
+#     except KeyboardInterrupt:
+#         print("\tinterrupted during initialization")
+#         pass
+
 #     while True:
-#         if(bat.ready):
-#             print("bat is ready")
-#             print("\tTime is: {}".format(bat.counter.t_now))
-#             c.blue.battery_service.level = bat.get_percent()
-# except KeyboardInterrupt:
-#     print("\tinterrupted during gesture detection")
+#         try:
+#             await asyncio.sleep(0.1)
+#             c.blue.battery_service.level = c.battery.get_percent()
+#             c.move_mouse() #instead of moving mouse as method, have it be blocked on async when not in use
+#             c.dispatch_event( c.detect_event() ) # dispatch event should always be running
+#         except:
+#             break
 
-asyncio.run( main() )
+# asyncio.run( main() )
 
-print("\nPROGRAM COMPLETE.\n")
+# print("\nPROGRAM COMPLETE.\n")
 
-mode.select_reboot_mode()
+# mode.select_reboot_mode()
