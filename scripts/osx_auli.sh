c041cc345eca9948410a8ae954434e8400c4d293
Spero Koulouras <spero@auli.tech>
2023-05-10 12:30:18 -0700

    add scripts for osx installs

---
+++ scripts/osx_auli.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+device="Cato"
+boot_mount="XIAO-SENSE"
+auli_mount="AULI_CATO"
+windows_mount="/d/"
+WHITELIST="./scripts/whitelist.txt"
+
+await_test () {
+    # Prompt if test passes
+    [ $2 "$3" ] && echo -e "$1"
+
+    # Wait for test to pass
+    ct=0
+    while [ $2 "$3" ]
+    do
+        sleep 1
+        ct=$((ct+1))
+        echo -n "$ct "
+        [ $ct -gt $4 ] && echo -e "\nTimed out. $5" && exit 1
+    done
+}
+
+# osx or windows?
+
+os=$(uname -s) || $(ver) || true
+if [ "$os" == "" ]; then
+    echo "Failed. Can't determine OS."
+    exit
+fi
+
+if [ "$os" == "Darwin" ]; then
+    os="OSX"
+    auli_cato_loc="/Volumes/$auli_mount"
+    boot_load_loc="/Volumes/$boot_mount"
+else
+    os="WINDOWS"
+    auli_cato_loc=$windows_mount
+    boot_load_loc=$windows_mount
+fi
+
+# Presence of boot_out.txt indicates board is in circuitPython Config
+FILE1=$auli_cato_loc/boot_out.txt
+
+# Presence of these three files indicates default bootloader config
+BLFILE1=$boot_load_loc/CURRENT.UF2
+BLFILE2=$boot_load_loc/INDEX.HTM
+BLFILE3=$boot_load_loc/INFO_UF2.TXT
+
+# Make sure update files are present
+
+[ ! -f .bootloader/*.uf2 ] && echo "Firmare not found." && exit 1
+
+[ ! -f $WHITELIST ] && echo "Application Files not found." && exit 1
+
+echo Installing Application on $auli_cato_loc
+
+await_test "Waiting for $device." "! -d" "$auli_cato_loc" 30 "$device not ready. Please reset with a single tap."
+
+#
+echo "Downloading Libraries"
+echo "    Copying Libraries"
+
+# Make sure lib directory exists
+[ ! -d "$auli_cato_loc/lib" ] && mkdir "$auli_cato_loc/lib"
+
+for dir in lib/**
+do
+    dir=$dir
+    echo "        ${dir}"
+    cp -r $dir "$auli_cato_loc/lib"
+done
+
+echo Installing $device Application files on $auli_cato_loc
+await_test "Checking for $device." "! -d" "$auli_cato_loc" 30 "Please reset $device with a single tap."
+
+echo "Copying Files:"
+while read -r line
+do
+    line=`echo $line | sed 's/\r//'`
+    echo "    $line"
+    cp "$line" "$auli_cato_loc"
+done < "$WHITELIST"
