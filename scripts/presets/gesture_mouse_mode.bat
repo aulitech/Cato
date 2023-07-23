49100c0a7d5a39f7a225f91c3bd466e4066f5787
Finn Biggs <finn@auli.tech>
2023-07-22 20:30:17 -0700

    Added preset batches for easy demo.
    Battery speed increased
    
    Binding adjustment

---
+++ scripts/presets/gesture_mouse_mode.bat
@@ -0,0 +1,5 @@
+@echo on
+cd
+Rem Copies an existing tv_remote preset onto an attached Cato device
+
+copy /Y "C:\Users\finnb\Documents\auli\Cato\scripts\presets\configs\gesture_mouse.json" "D:\config.json"
