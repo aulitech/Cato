e09a7ef77d77133263e59442f97c3ed51c5e3e19
Finn Biggs <finn@auli.tech>
2023-09-19 18:06:54 -0700

    Update structure of release subfolder

--- scripts/update_drag_drop.sh
+++ scripts/update_drag_drop.sh
@@ -1,16 +1,27 @@
 #! /usr/bin/bash
 
 # This script is designed to update the contents of "DragAndDrop_ToProgram"
-drag_drop_loc="DragAndDrop_ToProgram/"
+release_loc="release/"
+uf2_loc="release/firmware/"
+code_loc="./release/drag_and_drop/"
 
 # reset the release folder
-rm -rf $drag_drop_loc
-mkdir $drag_drop_loc
+rm -rf $release_loc
+
+mkdir $release_loc
+mkdir $uf2_loc
+mkdir $code_loc
 
 WHITELIST="./scripts/whitelist.txt"
     while read -r line
     do
         line=${line:0:-1}
         echo "    Copying $line to Drag/Drop Folder"
-        cp $line $drag_drop_loc
+        cp $line $code_loc
     done < "$WHITELIST"
+
+# Copy Libs
+cp -r "./lib" $code_loc
+
+# Copy bootloader
+cp ./.bootloader/* $uf2_loc
